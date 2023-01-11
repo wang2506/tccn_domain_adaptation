0e81f0ac38ba6bbb6efdea07c1b465ffb6779ae9
henry <wang2506@purdue.edu>
2023-01-11 18:01:03 -0500

    a

--- mt_runs.py
+++ mt_runs.py
@@ -2,12 +2,12 @@ import os
 
 ##### https://stackoverflow.com/questions/51232600/
 
-# args = [1,2,3,4,5]
-# for arg in args:
-#     os.system("python main_mt.py --seed {}".format(arg))
-
-args = [2,3,4,5]
+args = [2,3,4,5] # 1
 for arg in args:
-    os.system("python ./optim_prob/div_calc.py --seed {}".format(arg))
-    os.system("python ./optim_prob/main_script_phi_e.py --seed {}".format(arg))
-    os.system("python ./main_mt.py --seed {}".format(arg))
+    os.system("python main_mt.py --seed {}".format(arg))
+
+# args = [2,3,4,5]
+# for arg in args:
+#     os.system("python ./optim_prob/div_calc.py --seed {}".format(arg))
+#     os.system("python ./optim_prob/main_script_phi_e.py --seed {}".format(arg))
+#     os.system("python ./main_mt.py --seed {}".format(arg))
