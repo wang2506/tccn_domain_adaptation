75962511879be7f85ae341bb22be5bec879e9ff8
henry <wang2506@purdue.edu>
2023-01-11 12:30:56 -0500

    a

--- optim_prob/div_utils/neural_nets.py
+++ optim_prob/div_utils/neural_nets.py
@@ -426,7 +426,7 @@ def fl_subprocess(ld_sets,args,d_train,nnet,device):
     
     for t in range(1,round(args.st_time/(num_aggs*agg_period))): #+more_lt):
     # for t in range(1,num_aggs): #+more_lt):
-        if t <= round(args.st_time/(num_aggs*agg_period)): #t%agg_period == 0 and        
+        # if t <= round(args.st_time/(num_aggs*agg_period)): #t%agg_period == 0 and        
         ## train
         all_w = []
         for i in range(args.l_devices):
