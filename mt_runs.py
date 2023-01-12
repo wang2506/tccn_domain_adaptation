9be2ca832ec90f522efa2c9f6d0bd2d1b2dc6e6a
henry <wang2506@purdue.edu>
2023-01-11 20:57:30 -0500

    a

--- mt_runs.py
+++ mt_runs.py
@@ -2,7 +2,7 @@ import os
 
 ##### https://stackoverflow.com/questions/51232600/
 
-args = [2,3,4,5] # 1
+args = [1,2,3,4,5]
 for arg in args:
     os.system("python main_mt.py --seed {}".format(arg))
 
