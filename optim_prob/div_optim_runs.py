9e7d8d2a6b2343001488b70f0e8b588b6fdb18ce
wang2506@purdue.edu <wang2506@purdue.edu>
2023-01-13 02:58:12 +0000

    a

--- optim_prob/div_optim_runs.py
+++ optim_prob/div_optim_runs.py
@@ -3,7 +3,7 @@ import os
 ##### https://stackoverflow.com/questions/51232600/
 
 # args = [1]#,2,3,4,5]
-args = [1]
+args = [5]
 for arg in args:
     os.system("python div_calc.py --seed {}".format(arg))
     os.system("python main_script_phi_e.py --seed {}".format(arg))
