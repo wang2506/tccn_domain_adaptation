12548c9c52f481cfedc2f54b50ec24f1bfdd06ae
wang2506@purdue.edu <wang2506@purdue.edu>
2023-03-05 03:58:43 +0000

    dset2,M+MM

--- optim_prob/div_optim_runs.py
+++ optim_prob/div_optim_runs.py
@@ -2,7 +2,7 @@ import os
 
 ##### https://stackoverflow.com/questions/51232600/
 
-args = [1]#,2,3,4,5]
+args = [5]#,2,3,4,5]
 # args = [5]
 for arg in args:
     os.system("python div_calc.py --seed {}".format(arg))
